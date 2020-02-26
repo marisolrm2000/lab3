f6db3e01fd6948c3c78eba378a71ae0de7826945
Marisol Mattson <marisolrm2000@gmail.com>
2020-02-26 15:32:02 -0700

    poooop

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
-    if(isset($_SESSION['loggedin'] != 1)){ //if login in session is not set
-      header("Location:../login.php");
+    if($_SESSION['loggedin'] == 0){ //if login in session is not set
+      header("Location:login.php");
   }
 ?>
 <!DOCTYPE html>
